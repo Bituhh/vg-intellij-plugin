55eceff949d8e846e0ca88e8865e278cd3f1d685
Victor Oliveira <victor@oliveira.org.uk>
2021-05-27 01:42:44 +0100

    Fixing file changes dialog

--- src/main/java/uk/org/oliveira/vg/version_json/FileChangeListener.java
+++ src/main/java/uk/org/oliveira/vg/version_json/FileChangeListener.java
@@ -3,112 +3,70 @@ package uk.org.oliveira.vg.version_json;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiTreeChangeEvent;
-import com.intellij.psi.PsiTreeChangeListener;
+import com.intellij.openapi.vfs.newvfs.BulkFileListener;
+import com.intellij.openapi.vfs.newvfs.events.VFileEvent;
 import net.minidev.json.parser.ParseException;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.List;
 
-public class FileChangeListener implements PsiTreeChangeListener {
-    @Override
-    public void beforeChildAddition(@NotNull PsiTreeChangeEvent event) {
-
+public class FileChangeListener implements BulkFileListener {
+    private enum FileChangeOperation {
+        ADD, REMOVE
     }
 
     @Override
-    public void beforeChildRemoval(@NotNull PsiTreeChangeEvent event) {
-        editCurrentFolderBasedOnFile(event, "Do you wish to remove this file from 'version.json'?", file -> {
-            try {
-                VersionJsonManager.removeFile(file);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        });
-    }
+    public void after(@NotNull List<? extends VFileEvent> events) {
+        for (VFileEvent vFileEvent : events) {
+            boolean isCreated = vFileEvent.toString().contains("create");
+            boolean isDeleted = vFileEvent.toString().contains("deleted");
+            boolean isChangedFromOutside = vFileEvent.isFromRefresh() || vFileEvent.isFromSave();
 
-    @Override
-    public void beforeChildReplacement(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    @Override
-    public void beforeChildMovement(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    @Override
-    public void beforeChildrenChange(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    @Override
-    public void beforePropertyChange(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    @Override
-    public void childAdded(@NotNull PsiTreeChangeEvent event) {
-        editCurrentFolderBasedOnFile(event, "Do you wish to add this file to 'version.json'?", file -> {
-            try {
-                VersionJsonManager.addFile(file);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        });
-    }
-
-    @Override
-    public void childRemoved(@NotNull PsiTreeChangeEvent event) {
-    }
-
-    @Override
-    public void childReplaced(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    @Override
-    public void childrenChanged(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    @Override
-    public void childMoved(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    @Override
-    public void propertyChanged(@NotNull PsiTreeChangeEvent event) {
-
-    }
-
-    private interface FileChangeCallback {
-        void run(VirtualFile file) throws IOException;
-    }
-
-    private static void editCurrentFolderBasedOnFile(@NotNull PsiTreeChangeEvent event, String message, FileChangeCallback operation) {
-        PsiElement child = event.getChild();
-        PsiFile containingFile = child.getContainingFile();
-        if (containingFile != null) {
-            VirtualFile file = containingFile.getVirtualFile();
-            if (file != null) {
-                if (file.getPath().contains("/postgres/release/current") && !file.getName().equals("version.json")) {
-                    ApplicationManager.getApplication().invokeLater(() -> {
-                        String title = "Current Folder Edited";
-                        String[] buttons = new String[]{Messages.getYesButton(), Messages.getNoButton()};
-                        boolean isYes = Messages.showDialog(message, title, buttons, 0, Messages.getQuestionIcon()) == 0;
-                        if (isYes) {
-                            try {
-                                operation.run(file);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
+            if ((isCreated || isDeleted) && !isChangedFromOutside) {
+                VirtualFile virtualFile = vFileEvent.getFile();
+                if (virtualFile != null) {
+                    if (isCreated) {
+                        operateOnCurrentFolder(
+                                virtualFile.getName().concat(" was created! Do you wish to add this file to 'version.json'?"),
+                                FileChangeOperation.ADD,
+                                virtualFile
+                        );
+                    }
+                    if (isDeleted) {
+                        operateOnCurrentFolder(
+                                virtualFile.getName().concat(" was deleted! Do you wish to remove this file from 'version.json'?"),
+                                FileChangeOperation.REMOVE,
+                                virtualFile
+                        );
+                    }
                 }
             }
+
+        }
+    }
+
+    private static void operateOnCurrentFolder(@NotNull String message, @NotNull FileChangeOperation operation, @NotNull VirtualFile file) {
+        if (file.getPath().contains("/postgres/release/current") && !file.getName().equals("version.json")) {
+            ApplicationManager.getApplication().invokeLater(() -> {
+                String title = "Current Folder Edited";
+                String[] buttons = new String[]{Messages.getYesButton(), Messages.getNoButton()};
+                boolean isYes = Messages.showDialog(message, title, buttons, 0, Messages.getQuestionIcon()) == 0;
+                if (isYes) {
+                    try {
+                        switch (operation) {
+                            case ADD:
+                                VersionJsonManager.addFile(file);
+                                break;
+                            case REMOVE:
+                                VersionJsonManager.removeFile(file);
+                                break;
+                        }
+                    } catch (ParseException | IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         }
     }
 }
